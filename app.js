6e5477297b5f5d3a23a72b1fa71c0f3d477f0a1c
stephenliu <stephenlyao@yeah.net>
2016-08-21 11:46:19 +0800

    change port

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ var app = express();
 
 
 // view engine setup
-app.set('port',process.env.PORT || 3000);
+app.set('port',process.env.PORT || 3001);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
